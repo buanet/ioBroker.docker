f841ccf882ccf68c0765c17375bf86af363acaf7
Andre <buanet@users.noreply.github.com>
2017-12-01 23:06:34 +0100

    Update iobroker_startup.sh

--- scripts/iobroker_startup.sh
+++ scripts/iobroker_startup.sh
@@ -10,5 +10,5 @@ fi
                                                                                                                                   
 /opt/scripts/avahi_startup.sh                                                                                                     
 sleep 5                                                                                                                           
-node node_modules/iobroker.js-controller/controller.js >/opt/transfer/docker_log.txt 2>&1 &                                       
+node node_modules/iobroker.js-controller/controller.js >/opt/scripts/docker_iobroker_log.txt 2>&1 &                                       
 /bin/bash
