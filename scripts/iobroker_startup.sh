0cc07e148d7e5990599640bc955787bcd6b1fafb
Andre <buanet@users.noreply.github.com>
2017-12-01 23:41:15 +0100

    Update iobroker_startup.sh

--- scripts/iobroker_startup.sh
+++ scripts/iobroker_startup.sh
@@ -1,14 +1,15 @@
-#!/bin/sh                                                                                                                         
-                                                                                                                                  
-cd /opt/iobroker                                                                                                                  
-                                                                                                                                  
-if [ -f .install_host ];                                                                                                          
-then                                                                                                                              
-        ./iobroker host $(cat .install_host) && echo $(hostname) > .install_host                                                  
-        rm .install_host                                                                                                          
-fi                                                                                                                                
-                                                                                                                                  
-/opt/scripts/avahi_startup.sh                                                                                                     
-sleep 5                                                                                                                           
-node node_modules/iobroker.js-controller/controller.js >/opt/scripts/docker_iobroker_log.txt 2>&1 &                                       
+#!/bin/sh
+
+cd /opt/iobroker
+
+if [ -f .install_host ];
+then
+        echo $(hostname) > .install_host
+        ./iobroker host $(cat .install_host)
+        rm .install_host
+fi
+
+/opt/scripts/avahi_startup.sh
+sleep 5
+node node_modules/iobroker.js-controller/controller.js >/opt/scripts/docker_iobroker_log.txt 2>&1 &
 /bin/bash
