b8622098b22a2113322fbea9c1b9c915d92185de
Andre <buanet@users.noreply.github.com>
2017-12-01 23:01:46 +0100

    Update iobroker_startup.sh

--- scripts/iobroker_startup.sh
+++ scripts/iobroker_startup.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
-
-cd /opt/iobroker
-
-if [ -f .install_host ];
-then
-	./iobroker host $(cat .install_host) && echo $(hostname) > .install_host
-	rm .install_host
-fi
-
-/opt/scripts/avahi_startup.sh
-sleep 5
-node node_modules/iobroker.js-controller/controller.js
+#!/bin/sh                                                                                                                         
+                                                                                                                                  
+cd /opt/iobroker                                                                                                                  
+                                                                                                                                  
+if [ -f .install_host ];                                                                                                          
+then                                                                                                                              
+        ./iobroker host $(cat .install_host) && echo $(hostname) > .install_host                                                  
+        rm .install_host                                                                                                          
+fi                                                                                                                                
+                                                                                                                                  
+/opt/scripts/avahi_startup.sh                                                                                                     
+sleep 5                                                                                                                           
+node node_modules/iobroker.js-controller/controller.js >/opt/transfer/docker_log.txt 2>&1 &                                       
 /bin/bash
