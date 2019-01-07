7ced614900c538f5270d6aa814dceb7092e80ad1
Andre <buanet@users.noreply.github.com>
2019-01-07 11:12:40 +0100

    added output for debugging

--- scripts/iobroker_startup.sh
+++ scripts/iobroker_startup.sh
@@ -4,13 +4,22 @@ cd /opt/iobroker
 
 if [ -f .install_host ];
 then
+        echo 'First run preparation! Used Hostname:' $(hostname)
         echo $(hostname) > .install_host && ./iobroker host $(cat .install_host)
 		iobroker del admin.0 && iobroker del discovery.0
 		iobroker add admin && iobroker add discovery
         rm .install_host
+	echo 'First run Preparation done...'
 fi
 
-/opt/scripts/avahi_startup.sh
+echo 'Initializing Avahi-Daemon...'
+sh /opt/scripts/avahi_startup.sh
+echo 'Initializing Avahi-Daemon done...'
+
 sleep 5
+
+echo 'Starting ioBroker...'
 node node_modules/iobroker.js-controller/controller.js >/opt/scripts/docker_iobroker_log.txt 2>&1 &
+echo 'Starting ioBroker done...
+
 /bin/bash
