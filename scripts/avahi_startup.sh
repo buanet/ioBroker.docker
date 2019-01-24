d03ce6d24c1ba16f5615b6a168c07e3964b54dec
buanet <info@buanet.de>
2019-01-24 15:56:57 +0100

    fix

--- scripts/avahi_startup.sh
+++ scripts/avahi_startup.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+<<<<<<< refs/remotes/origin/master
 if [ -f /var/run/dbus/pid ];
 then
   rm -f /var/run/dbus/pid
 fi
 
+=======
+echo 'Preparing...'
+rm /var/run/dbus/pid
+>>>>>>> fix
 dbus-daemon --system
 
 echo 'Restarting...'
