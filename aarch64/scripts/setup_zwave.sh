af7b7ff6f01b746722644b37723887fa9314368e
andre <>
2020-01-17 23:36:28 +0100

    new openzwave version

--- aarch64/scripts/setup_zwave.sh
+++ aarch64/scripts/setup_zwave.sh
@@ -8,9 +8,9 @@ then
 else
   echo "Openzwave is NOT installed. Going to install it now..."
   cd /opt
-  curl -s -L -O http://old.openzwave.com/downloads/openzwave-1.6.945.tar.gz
-  tar -xf openzwave-1.6.945.tar.gz && rm openzwave-1.6.945.tar.gz
-  cd openzwave-1.6.945 && make > /dev/null 2>&1 && make install > /dev/null 2>&1
+  curl -s -L -O http://old.openzwave.com/downloads/openzwave-1.6.1007.tar.gz
+  tar -xf openzwave-1.6.1007.tar.gz && rm openzwave-1.6.1007.tar.gz
+  cd openzwave-1.6.1007 && make > /dev/null 2>&1 && make install > /dev/null 2>&1
   ldconfig /usr/local/lib64
   cd /opt/iobroker
   # echo "Openzwave is now installed..."
