4eb518ca5e4f1bdd983f292f93ef295cae595937
thost96 <info@thorstenreichelt.de>
2020-02-20 23:56:22 -0800

    changed startup and log output

--- Dockerfile
+++ Dockerfile
@@ -25,14 +25,6 @@ RUN sed -i -e 's/# de_DE.UTF-8 UTF-8/de_DE.UTF-8 UTF-8/' /etc/locale.gen \
     && \update-locale LANG=de_DE.UTF-8
 RUN cp /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
-RUN mkdir -p /opt/scripts/ \
-    && chmod 777 /opt/scripts/
-WORKDIR /opt/scripts/
-COPY scripts/iobroker_startup.sh iobroker_startup.sh
-COPY scripts/packages_install.sh packages_install.sh
-RUN chmod +x iobroker_startup.sh \
-	&& chmod +x packages_install.sh
-
 WORKDIR /
 RUN curl -sL https://raw.githubusercontent.com/ioBroker/ioBroker/stable-installer/installer.sh | bash - \
     && echo $(hostname) > /opt/iobroker/.install_host \
@@ -41,18 +33,24 @@ RUN curl -sL https://raw.githubusercontent.com/ioBroker/ioBroker/stable-installe
 WORKDIR /opt/iobroker/
 RUN npm install node-gyp -g
 
-
-RUN tar -cf /opt/iobroker/initial_iobroker.tar /opt/iobroker
-
 RUN echo 'iobroker ALL=(ALL) NOPASSWD: ALL' | EDITOR='tee -a' visudo \
     && echo "iobroker:iobroker" | chpasswd \
     && adduser iobroker sudo
 
-USER iobroker
-
 ENV DEBIAN_FRONTEND="teletype" \
 	LANG="de_DE.UTF-8" \
 	TZ="Europe/Berlin"
+
+RUN ln -sf /dev/stdout /opt/iobroker/iobroker-daemon.log 
+
+RUN mkdir -p /opt/scripts/ && chmod -R 777 /opt/scripts/
+WORKDIR /opt/scripts/
+COPY scripts/iobroker_startup.sh iobroker_startup.sh
+RUN chmod +x iobroker_startup.sh
+
+WORKDIR /opt/iobroker/
+
+USER iobroker
 
 EXPOSE 8081/tcp
 VOLUME ["/opt/iobroker"]
--- scripts/iobroker_startup.sh
+++ scripts/iobroker_startup.sh
@@ -1,46 +1,24 @@
 #!/bin/bash
 
-# Reading env-variables
-packages=$PACKAGES
-
 # Version output for logging
-echo 'Version: 2.0.5beta'
+echo 'Version: 3.0.0'
 echo 'Startupscript running...'
 
-# Checking and installing additional packages
-if [ "$packages" != "" ]
-then
-  echo 'Installing additional packages...'
-  echo 'The following packages will be installed:' $packages
-  sudo echo $packages > /opt/scripts/.packages
-  sudo sh /opt/scripts/packages_install.sh > /opt/scripts/packages_install.log 2>&1
-  echo 'Installing additional packages done...'
-fi
-
 cd /opt/iobroker
-
-# Checking and restoring ioBroker to mounted folder
-if [ `ls -1a|wc -l` -lt 3 ]
-then
-  echo 'Directory /opt/iobroker is empty!'
-  echo 'Restoring...'
-	sudo tar -xf /opt/initial_iobroker.tar -C /
-	echo 'Restoring done...'
-fi
 
 # Checking for first run and renaming ioBroker
 if [ -f /opt/iobroker/.install_host ]
 then
   echo 'First run preparation! Used Hostname:' $(hostname)
-	echo 'Renaming ioBroker...'
+  echo 'Renaming ioBroker...'
   iobroker host $(cat /opt/iobroker/.install_host)
   sudo rm -f /opt/iobroker/.install_host
-	echo 'First run preparation done...'
+  echo 'First run preparation done...'
 fi
 
 # Starting ioBroker
 echo 'Starting ioBroker...'
-sudo node node_modules/iobroker.js-controller/controller.js > /opt/scripts/iobroker.log 2>&1 &
+sudo node node_modules/iobroker.js-controller/controller.js > /opt/iobroker/iobroker-daemon.log 2>&1 &
 echo 'Starting ioBroker done...'
 
 # Preventing container restart
