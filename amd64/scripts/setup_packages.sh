0621c8d8b24b65fb2f8f52619a62572132ba982b
buanet <info@buanet.de>
2021-08-28 19:11:30 +0200

    small fix

--- amd64/scripts/setup_packages.sh
+++ amd64/scripts/setup_packages.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 if [ $1 == "-install" ]
-  then
-    apt-get -qq update
-    packages=$(cat /opt/scripts/.packages)
-    for i in $packages; do
+then
+  apt-get -qq update
+  packages=$(cat /opt/scripts/.packages)
+  for i in $packages; do
     sudo apt-get -qq -y install $i
-    done
-  elif [ $1 == "-update" ]
-    apt-get -qq update
-    apt-get -qq -y upgrade
+  done
+elif [ $1 == "-update" ]
+then
+  apt-get -qq update
+  apt-get -qq -y upgrade
+else
+  echo "No paramerter found!"
+  exit 1
 fi
 
 rm -rf /var/lib/apt/lists/*
