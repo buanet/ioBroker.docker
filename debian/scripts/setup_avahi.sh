27aa771e2890b0e9503a48e3301f208e32889263
buanet <andre@buanet.de>
2022-06-16 09:45:39 +0200

    fix avahi logging

--- debian/scripts/setup_avahi.sh
+++ debian/scripts/setup_avahi.sh
@@ -27,11 +27,11 @@ then
   rm -f /var/run/avahi-daemon//pid
 fi
 
-echo -n "Starting dbus... "
+echo -n '[setup_avahi.sh] Starting dbus... '
   dbus-daemon --system >> /opt/scripts/avahi_startup.log 2>&1
 echo 'Done.'
 
-echo -n "Starting avahi-daemon... "
+echo -n '[setup_avahi.sh] Starting avahi-daemon... '
   /etc/init.d/avahi-daemon start >> /opt/scripts/avahi_startup.log 2>&1
 echo 'Done.'
 
