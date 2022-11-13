b18d22ddf8ff6ffad96187dfa4a0cf71775f09b9
buanet <info@buanet.de>
2022-11-13 20:51:11 +0100

    Revert "switch from apt-get to apt"
    This reverts commit eba7e6c5f9c276ff7773f793238357323d99c34a.

--- debian/scripts/setup_packages.sh
+++ debian/scripts/setup_packages.sh
@@ -2,13 +2,13 @@
 
 if [ $1 == "-install" ]
 then
-  apt -qq update
+  apt-get -qq update
   packages=$(cat /opt/scripts/.docker_config/.packages)
   for i in $packages; do
     if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ];
       then
         echo "$i is not installed. Installing..."
-        sudo apt -qq -y install $i
+        sudo apt-get -qq -y install $i
         echo "Done."
       else
         echo "$i is already installed."
@@ -16,15 +16,15 @@ then
   done
 elif [ $1 == "-update" ]
 then
-  apt -qq update
-  apt -qq -y upgrade
+  apt-get -qq update
+  apt-get -qq -y upgrade
 else
   echo "No paramerter found!"
   exit 1
 fi
 
 # Silent Cleanup
-apt -qq autoclean -y && apt -qq autoremove && apt -qq clean
+apt-get -qq autoclean -y && apt-get -qq autoremove && apt-get -qq clean
 rm -rf /tmp/* /var/tmp/* && rm -rf /root/.cache/* && rm -rf /var/lib/apt/lists/* && rm -f /opt/scripts/.docker_config/.packages
 
 exit 0
